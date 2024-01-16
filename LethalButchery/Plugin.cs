4181273abd0b4140f7fbc292e362cde1257a4d9e
Skye Brady <skye@blueskye.co.uk>
2024-01-16 18:44:14 +0000

    Set up assemblies

--- LethalButchery/Plugin.cs
+++ LethalButchery/Plugin.cs
@@ -8,7 +8,7 @@ namespace LethalButchery
         private void Awake()
         {
             // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} {PluginInfo.PLUGIN_VERSION} is loaded!");
         }
     }
 }
